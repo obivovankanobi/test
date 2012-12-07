c4ae63e102e627bf3f4b5353b0455cadbc8b3135
Vladimir Matiuha <chrono@tadatuta.com>
2012-12-07 15:58:23 +0200

    link bemhtml

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -14,67 +14,113 @@
             content: [
                 {
                     elem: 'slide',
-                    content: 'Матюха Владимир Петрович'
+                    content: 'Матюха Владимир Петрович.'
                 },
                 {
                     elem: 'slide',
-                    content: '1990'
+                    content: '1990.'
                 },
                 {
                     elem: 'slide',
-                    content: 'Ялта'
+                    content: 'Ялта.'
                 },
                 {
                     elem: 'slide',
-                    content: 'Ялтинский филиал Европейского университета,'
+                    content: 'Ялтинский филиал Европейского университета, информационных систем и тахнологий, информационные управляющие системы и технологии.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: '2013.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Ну могу кое как переписываться, читать документацию.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Прежде всего прокачать знания, заполненить пробелы и освоить что-то новое.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Узнал от друзей.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Часов 4-6, пока пишу диплом.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Занимался разработкой и поддержкой сайтов на WP, так же аутсорсил для яндекса (пару мелких промо сайтов), ещё немного участвовал в разработке десктопного сервиса Яндекс.Такси(фиксил баги вёрстки). Ну и само собой всякие сайты для курсовых и дипломной работы'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Всякие WP API, javascript(jquery), ajax, немного xml и xsl, php(обычно процедурный подход, но обучался и ООП), для разработки дипломной работы использовал twitter bootstrap и api Яндекс.Карт( приложение заюирает с изображения exif данные и создаёт отметку на карте, где было сделано фото), bem( bemjson, самую малость bemhtml).'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Начинал с Dreamveawer, потом notepad ++, phpstorm/webstorm, в настоящее время sublime text 2(лёгкий, плагины, модный, молодёжный)'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Windows Xp/7/8, минимально с mac и ubuntu. Windows как нестранно для работы так как на убунту нету фотошопа а на макосины не насобирал ещё.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Только с Git.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: 'Mantis, Мегаплан, Jira.'
                 },
                 {
                     elem: 'slide',
-                    content: 'ziga'
+                    content: [
+                        {
+                            block: 'menu-list',
+                            content: [
+                                {
+                                    elem: 'item',
+                                    content: [
+                                        {
+                                             block: 'link',
+                                            attrs: { href: 'http://css-tricks.com' },
+                                            content: 'css-tricks.com'
+                                        }
+                                    ]
+                                },
+                                {
+                                    elem: 'item',
+                                    content: [
+                                         {
+                                            block: 'link',
+                                            attrs: { href: 'http://www.smashingmagazine.com/' },
+                                            content: 'smashingmagazine.com'
+                                        }
+                                    ]
+                                },
+                                {
+                                    elem: 'item',
+                                    content: [
+                                        {
+                                            block: 'link',
+                                            attrs: { href: 'http://htmlbook.ru/' },
+                                            content: 'htmlbook.ru'
+                                        }
+                                    ]
+                                },
+                                {
+                                    elem: 'item',
+                                    content: [
+                                        {
+                                            block: 'link',
+                                            attrs: { href: 'http://caniuse.com/' },
+                                            content: 'caniuse.com'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }                        
+                    ] 
                 },
                 {
                     elem: 'slide',
