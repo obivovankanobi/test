23e0f88c985337d000cde35390614bb0382f45fe
obivovankanobi <ultrachronop@gmail.com>
2012-12-09 16:20:27 +0200

    return old animation

--- blocks-desktop/b-page/b-page.js
+++ blocks-desktop/b-page/b-page.js
@@ -1,4 +1,4 @@
-/*$(function(){
+$(function(){
 
 var animationFlag = false;
 
@@ -27,33 +27,5 @@ var animationFlag = false;
 
     $('.slider__button:first').click();  
   
-}) */
-
-$(function(){
-
-
-
-    $('.slider__button').live('click',function(){
-       
-          
-            slides = $('.slider__slide');
-            buttons = $('.slider__button, .slider__button_state_active');            
-            indexOfSlide = buttons.index($(this));
-            
-            $('.slider__slide_state_active').css({
-                marginLeft: '-50px'
-            });
-            $('.slider__slide_state_active').removeClass('slider__slide_state_active').addClass('slider__slide').hide();  
-            $('.slider__slide').eq(indexOfSlide).animate({
-                marginLeft: '50',            
-                opacity: 'toggle'                
-            }, 500 )
-            $('.slider__slide').eq(indexOfSlide).addClass('slider__slide_state_active');
-            $('.slider__button_state_active').removeClass('slider__button_state_active').addClass('slider__button');
-            $(this).addClass('slider__button_state_active').removeClass('slider__button');
-               
-    })
-
-    $('.slider__button:first').click();  
-  
 }) 
+
