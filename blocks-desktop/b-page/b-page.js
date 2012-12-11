61de82b7806a27581cd0ef6ba4312cfd52c3a457
obivovankanobi <ultrachronop@gmail.com>
2012-12-11 23:53:17 +0200

    no copypaste more js

--- blocks-desktop/b-page/b-page.js
+++ blocks-desktop/b-page/b-page.js
@@ -1,31 +1,44 @@
 $(function(){
 
-var animationFlag = false;
+var animationFlag = false, anwrs=[
+'Матюха Владимир Петрович.',
+'1990.',
+'Ялта.',
+'Ялтинский филиал Европейского университета, информационных систем и тахнологий, информационные управляющие системы и технологии, программист-инженер',
+'2013.',
+'Могу кое-как переписываться, читать документацию.',
+'Прежде всего прокачать знания, заполнить пробелы и освоить что-то новое.',
+'Узнал от друзей.',
+'По возможности всё.',
+'Занимался разработкой и поддержкой сайтов на WP, так же аутсорсил для яндекса (пару мелких промо сайтов), ещё немного участвовал в разработке десктопного сервиса Яндекс.Такси(фиксил баги вёрстки). Ну и само собой всякие сайты для курсовых и дипломной работы',
+'Wordpress API, javascript(jquery), ajax, немного xml и xsl, php(обычно процедурный подход, но обучался и ООП), для разработки дипломной работы использовал twitter bootstrap и api Яндекс.Карт( приложение забирает с изображения exif данные и создаёт отметку на карте, где было сделано фото), bem( bemjson, самую малость bemhtml).',
+'Начинал с Dreamveawer, потом notepad ++, phpstorm/webstorm, в настоящее время sublime text 2(лёгкий, плагины, модный, молодёжный).',
+'Windows Xp/7/8, минимально с mac и ubuntu. Windows как нестранно для работы так как на убунту нету фотошопа, а на mac не насобирал ещё.',
+'Только с Git.',
+'Mantis, Мегаплан, Jira.',
+'<ol class="menu-list"><li class="menu-list__item"><a class="link" href="http://css-tricks.com">css-tricks.com</a></li><li class="menu-list__item"><a class="link" href="http://www.smashingmagazine.com/">smashingmagazine.com</a></li><li class="menu-list__item"><a class="link" href="http://htmlbook.ru/">htmlbook.ru</a></li><li class="menu-list__item"><a class="link" href="http://caniuse.com/">caniuse.com</a></li></ol>',
+'В студии tadatuta. Вёрстка, js, разработка тем для wordpress и поддержка существующих сайтов. Есть опыт разработки в команде.'];
 
-    $('.slider__button').live('click',function(){
+   $('.slider__button').live('click',function(){
         if(!animationFlag){
             animationFlag = true;
             slides = $('.slider__slide');
             buttons = $('.slider__button, .slider__button_state_active');            
-            indexOfSlide = buttons.index($(this));
+            indexOfSlide = buttons.index($(this));          
             
-            $('.slider__slide_state_active').css({
+            $('.slider__slide').css({
                 marginLeft: '-40px'
-            });
-            $('.slider__slide_state_active').removeClass('slider__slide_state_active').addClass('slider__slide').hide();  
-            $('.slider__slide').eq(indexOfSlide).animate({
-                marginLeft: '40',            
+            });           
+            $('.slider__slide').hide().html(anwrs[indexOfSlide]).animate({
+                marginLeft: '40',           
                 opacity: 'toggle'                
-            }, 250, function(){
-               animationFlag = false;
-            })
-            $('.slider__slide').eq(indexOfSlide).addClass('slider__slide_state_active');
+            }, 300, function(){
+               animationFlag = false;               
+            });            
             $('.slider__button_state_active').removeClass('slider__button_state_active').addClass('slider__button');
             $(this).addClass('slider__button_state_active').removeClass('slider__button');
         }       
     })
-
     $('.slider__button:first').click();  
-  
 }) 
 
--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -1,6 +1,6 @@
 ({
     block: 'b-page',
-    title: 'BEM Server Motivator',
+    title: 'Анкета',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
@@ -15,116 +15,6 @@
                 {
                     elem: 'slide',
                     content: 'Матюха Владимир Петрович.'
-                },
-                {
-                    elem: 'slide',
-                    content: '1990.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Ялта.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Ялтинский филиал Европейского университета, информационных систем и тахнологий, информационные управляющие системы и технологии.'
-                },
-                {
-                    elem: 'slide',
-                    content: '2013.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Ну могу кое как переписываться, читать документацию.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Прежде всего прокачать знания, заполненить пробелы и освоить что-то новое.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Узнал от друзей.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Часов 4-6, пока пишу диплом.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Занимался разработкой и поддержкой сайтов на WP, так же аутсорсил для яндекса (пару мелких промо сайтов), ещё немного участвовал в разработке десктопного сервиса Яндекс.Такси(фиксил баги вёрстки). Ну и само собой всякие сайты для курсовых и дипломной работы'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Всякие WP API, javascript(jquery), ajax, немного xml и xsl, php(обычно процедурный подход, но обучался и ООП), для разработки дипломной работы использовал twitter bootstrap и api Яндекс.Карт( приложение заюирает с изображения exif данные и создаёт отметку на карте, где было сделано фото), bem( bemjson, самую малость bemhtml).'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Начинал с Dreamveawer, потом notepad ++, phpstorm/webstorm, в настоящее время sublime text 2(лёгкий, плагины, модный, молодёжный)'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Windows Xp/7/8, минимально с mac и ubuntu. Windows как нестранно для работы так как на убунту нету фотошопа а на макосины не насобирал ещё.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Только с Git.'
-                },
-                {
-                    elem: 'slide',
-                    content: 'Mantis, Мегаплан, Jira.'
-                },
-                {
-                    elem: 'slide',
-                    content: [
-                        {
-                            block: 'menu-list',
-                            content: [
-                                {
-                                    elem: 'item',
-                                    content: [
-                                        {
-                                             block: 'link',
-                                            attrs: { href: 'http://css-tricks.com' },
-                                            content: 'css-tricks.com'
-                                        }
-                                    ]
-                                },
-                                {
-                                    elem: 'item',
-                                    content: [
-                                         {
-                                            block: 'link',
-                                            attrs: { href: 'http://www.smashingmagazine.com/' },
-                                            content: 'smashingmagazine.com'
-                                        }
-                                    ]
-                                },
-                                {
-                                    elem: 'item',
-                                    content: [
-                                        {
-                                            block: 'link',
-                                            attrs: { href: 'http://htmlbook.ru/' },
-                                            content: 'htmlbook.ru'
-                                        }
-                                    ]
-                                },
-                                {
-                                    elem: 'item',
-                                    content: [
-                                        {
-                                            block: 'link',
-                                            attrs: { href: 'http://caniuse.com/' },
-                                            content: 'caniuse.com'
-                                        }
-                                    ]
-                                }
-                            ]
-                        }                        
-                    ] 
-                },
-                {
-                    elem: 'slide',
-                    content: 'В студии tadatuta. Вёрстка, js, разработка тем для wordpress и поддержка существующих сайтов. Есть небольшой опыт разработки в команде.'
                 }
             ]
         },
