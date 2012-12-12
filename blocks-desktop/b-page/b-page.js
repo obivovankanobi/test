9b0508a8eb2c4dbeacb4c1cdcd0fe708a84425e2
obivovankanobi <ultrachronop@gmail.com>
2012-12-12 16:50:21 +0200

    text fix

--- blocks-desktop/b-page/b-page.js
+++ blocks-desktop/b-page/b-page.js
@@ -10,7 +10,7 @@ $(function(){
     'Прежде всего прокачать знания, заполнить пробелы и освоить что-то новое.',
     'Узнал от друзей.',
     'По возможности всё.',
-    'Занимался разработкой и поддержкой сайтов на WP, так же аутсорсил для яндекса (пару мелких промо сайтов), ещё немного участвовал в разработке десктопного сервиса Яндекс.Такси(фиксил баги вёрстки). Ну и само собой всякие сайты для курсовых и дипломной работы',
+    'Занимался разработкой и поддержкой сайтов на WP, так же аутсорсил для яндекса (пару мелких промо сайтов), ещё немного участвовал в разработке десктопного сервиса Яндекс.Такси(фиксил баги вёрстки). Ну и само собой всякие сайты для курсовых и дипломной работы.',
     'Wordpress API, javascript(jquery), ajax, немного xml и xsl, php(обычно процедурный подход, но обучался и ООП), для разработки дипломной работы использовал twitter bootstrap и api Яндекс.Карт( приложение забирает с изображения exif данные и создаёт отметку на карте, где было сделано фото), bem( bemjson, самую малость bemhtml).',
     'Начинал с Dreamveawer, потом notepad ++, phpstorm/webstorm, в настоящее время sublime text 2(лёгкий, плагины, модный, молодёжный).',
     'Windows Xp/7/8, минимально с mac и ubuntu. Windows как нестранно для работы так как на убунту нету фотошопа, а на mac не насобирал ещё.',
@@ -32,8 +32,7 @@ $(function(){
             
             if ( indexOfSlide == buttons.length-1 || indexOfSlide == buttons.length-2 ){
                 topOffset = topOffset - 60;                 
-            }
-           
+            }           
             
             $('.slider').css({
                 marginLeft: '-40px'
@@ -50,9 +49,7 @@ $(function(){
             $(this).addClass('slider__button_state_active').removeClass('slider__button');
         }       
     })
-    $('.slider__button:first').click(); 
- 
-
+    $('.slider__button:first').click();
 
     $(window).resize(function (event) {
         var currentButtonTopOffset = $('.slider__button_state_active').offset().top;
@@ -60,9 +57,7 @@ $(function(){
         $('.slider').css({
             marginTop: currentButtonTopOffset
         })
-        /*$('.slider').animate({
-          
-        });*/
+
     }); 
 
 }) 
